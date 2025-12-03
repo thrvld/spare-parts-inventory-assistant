317891d94a6a48e50e7ad3fd9a3306ce84640e14
Thor-not-vald <s255388@dtu.dk>
2025-12-03 14:13:03 +0100

    done

--- src/main.c
+++ src/main.c
@@ -1,28 +1,46 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define LINE_SIZE 30
+#define true 1
+#define false 0
+#define LINE_SIZE 40
 #define PART_COUNT 3
+#define count_of(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-char line[LINE_SIZE];
-const char MESSAGE_WELCOME[] = "Hello. Welcome to the spare parts inventory!.\n";
-const char MESSAGE_NEED[] = "Which part do you need?  ";
-const char PARTS_AVAILABLE[][LINE_SIZE] = {"hydraulic pump", "PLC module",
-                                           "servo motor"};
-char MESSAGE_POSITIVE[] = "I've got a here for you :). Bye!";
-char MESSAGE_NEGATIVE[] = "I don't have that in stock.";
-// char MESSAGE_STOCK[] = ""
+char INPUT[LINE_SIZE];
+const char PARTS[PART_COUNT][LINE_SIZE] = { "hydraulic pump", 
+                                            "PLC module", 
+                                            "servo motor" };
+const char MESSAGE_WELCOME[] = "Welcome to the spare parts inventory!\n"
+                               "Which part are you looking for?\n";
+const char MESSAGE_ENTER[] = "Enter desired part:    ";
+const char MESSAGE_POSITIVE[] = "Here is the part you asked for :)\nBye!\n";
+const char MESSAGE_NEGATIVE[] = "Sorry - I don't have that in stock.\n"
+                                "What else would you like me to look for?\n";
 
 int main(void) {
-  printf(MESSAGE_WELCOME) printf(MESSAGE_NEED);
-  scanf(PARTS_AVAILABLE, line);
-  for (size_t i = 0; i < _Countof PARTS_AVAILABLE; ++i)
-    if (strcmp(line, PARTS_AVAILABLE[i]) == 0) {
-      puts(MESSAGE_POSITIVE);
-      return EXIT_SUCCESS;
+    printf("%s%s", MESSAGE_WELCOME, MESSAGE_ENTER);
+
+    int found = false;
+
+    while (!found) {
+        if (fgets(INPUT, count_of(INPUT), stdin)) {
+            INPUT[strcspn(INPUT, "\n")] = 0;
+        }
+
+        int i = false;
+
+        while (i < PART_COUNT && strcmp(INPUT, PARTS[i]) != 0) {
+            i++;
+        }
+
+        if (i < PART_COUNT) {
+            found = true;
+
+        } else {
+            printf("%s%s", MESSAGE_NEGATIVE, MESSAGE_ENTER);
+        }
     }
-  puts(MESSAGE_NEGATIVE);
-  return
+    printf("%s", MESSAGE_POSITIVE);
 }
